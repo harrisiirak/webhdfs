8f2394bc7fe25bd48f29b15e96ddaa5873db6b4e
Harri Siirak <harri.siirak@gmail.com>
2014-02-12 00:35:53 +0200

    Delete operation support

--- test/webhdfs.js
+++ test/webhdfs.js
@@ -9,10 +9,12 @@ describe('WebHDFS', function () {
   });
 
   it('should make a directory', function (done) {
-    fs.mkdir('/temp', function () {
+    fs.mkdir('/tmp/path', function (err) {
+      demand(err).be.null();
       done();
     });
   });
+
   it('should list directory status', function () {});
   it('should create and write to a file', function () {});
   it('should open and read a file', function () {});
@@ -21,8 +23,10 @@ describe('WebHDFS', function () {
   it('should create symlink to file', function () {});
   it('should rename file', function () {});
   it('should delete file', function () {});
-  it('should delete directory', function () {
-    fs.unlink('/temp', function () {
+
+  it('should delete directory', function (done) {
+    fs.rmdir('/tmp/path', function (err) {
+      demand(err).be.null();
       done();
     });
   });
