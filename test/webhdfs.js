9a975a027d45ce98dafe86e649f214aa9aa0dda0
Harri Siirak <harri.siirak@gmail.com>
2014-02-12 21:43:06 +0200

    appendFile implementation

--- test/webhdfs.js
+++ test/webhdfs.js
@@ -11,6 +11,8 @@ describe('WebHDFS', function () {
     user: process.env.USER
   });
 
+  this.timeout(10000);
+
   it('should make a directory', function (done) {
     hdfs.mkdir(path, function (err) {
       demand(err).be.null();
@@ -22,6 +24,13 @@ describe('WebHDFS', function () {
 
   it('should create and write data to a file', function (done) {
     hdfs.writeFile(path + '/file-1', 'random data', function (err) {
+      demand(err).be.null();
+      done();
+    });
+  });
+
+  it('should append content to an existing file', function (done) {
+    hdfs.appendFile(path + '/file-1', 'more random data', function (err) {
       demand(err).be.null();
       done();
     });
@@ -41,8 +50,22 @@ describe('WebHDFS', function () {
     });
   });
 
+  it('should append stream content to an existing file', function (done) {
+    var localFileStream = fs.createReadStream(__filename);
+    var remoteFileStream = hdfs.createWriteStream(path + '/file-2', true);
+    var spy = sinon.spy();
+
+    localFileStream.pipe(remoteFileStream);
+    remoteFileStream.on('error', spy);
+
+    remoteFileStream.on('finish', function () {
+      demand(spy.called).be.falsy();
+      done();
+    });
+  });
+
   it('should open and read a file', function () {});
-  it('should append content to an existing file', function () {});
+  
   it('should change file permissions', function () {});
   it('should create symlink to file', function () {});
   it('should rename file', function () {});
