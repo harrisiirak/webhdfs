ba8a2ad782001b478afed0a17f8a8f14717d1168
Harri Siirak <harri.siirak@gmail.com>
2014-02-13 01:29:04 +0200

    Implemented readdir method

--- test/webhdfs.js
+++ test/webhdfs.js
@@ -20,8 +20,6 @@ describe('WebHDFS', function () {
     });
   });
 
-  it('should list directory status', function () {});
-
   it('should create and write data to a file', function (done) {
     hdfs.writeFile(path + '/file-1', 'random data', function (err) {
       demand(err).be.null();
@@ -90,9 +88,24 @@ describe('WebHDFS', function () {
     });
   });
 
+  it('should list directory status', function (done) {
+    hdfs.readdir(path, function (err, files) {
+      demand(err).be.null();
+      demand(files).have.length(2);
+
+      demand(files[0].pathSuffix).to.eql('file-1');
+      demand(files[1].pathSuffix).to.eql('file-2');
+
+      demand(files[0].type).to.eql('FILE');
+      demand(files[1].type).to.eql('FILE');
+      done();
+    });
+  });
+
   it('should change file permissions', function () {});
   it('should create symlink to file', function () {});
   it('should rename file', function () {});
+  it('should stat file', function () {});
   it('should delete file', function () {});
 
   it('should delete directory', function (done) {
